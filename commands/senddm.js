8e423d21970ab654b928240842cb3e0ca48ed7a2
Couteaucu <couteaucu@gmail.com>
2020-08-02 22:15:31 -0500

    added sent dm to message pipeline

--- commands/senddm.js
+++ commands/senddm.js
@@ -19,6 +19,40 @@ module.exports = {
             user.send(output);
             let kgbauthorized = message.guild.emojis.cache.find(emoji => emoji.name === 'kgbauthorized');
             await message.react(kgbauthorized).catch(() => message.channel.send("Acknowledged."));
+
+            client.channels.fetch('739624963530555504') //bot_dms TestDiscord
+                .then(channel => {
+                    const buildOuput = () => {
+                        var output_channel = "";
+                        output_channel += "--------------------------------\n";
+                        output_channel += `To: ${user.tag}`;
+
+                        if (output != '') {
+                            output_channel += "\n```";
+                            output_channel += output;
+                            output_channel += "```";
+                        }
+                        output_channel += "--------------------------------";
+
+                        return output_channel;
+                    }
+                    const sendfunction = async () => {
+                        try {
+                            const output_channel = buildOuput();
+                            await channel.send(output_channel);
+                            /* 								if (message.attachments.size > 0) {
+                                                                for (var object of message.attachments) {
+                                                                    var object = object[1];
+                                                                    await channel.send(object.proxyURL);
+                                                                }
+                                                            } */
+                        } catch{
+                            errorNotify('Function: DM Pipeline SendDM\nError: sending the message', message.guild);
+                        }
+                    }
+                    sendfunction();
+                })
+                .catch(console.error);
         } else {
             return message.reply('You are not authorized to use this command.');
         }
