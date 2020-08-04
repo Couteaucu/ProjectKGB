370018401d384b9d837a4bb723ac95bba7766492
Couteaucu <42109404+Couteaucu@users.noreply.github.com>
2020-08-04 15:44:57 -0500

    added basic quote function and regex (#7)

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const regex_nyaa = /n{1}y{1}a{1,2}/i;
 const regex_catgirl = /(cat){1}\s*(girl)+/i;
 const regex_foxgirl = /(fox){1}\s*(girl)+/i;
 const regex_neko = /(neko){1}(mimi)?/i;
+const regex_chase = /(chase){1}/i;
 const catRole = 'Nekomimi';
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -98,6 +99,8 @@ client.on('message', async message => {
 			} else if (regex_uwu.test(message.content)) {
 				//await message.channel.send('UwU');
 				await message.channel.send("Rawr x3 nuzzles how are you pounces on you you're so warm o3o notices you have a bulge o: someone's happy ;) nuzzles your necky wecky~ murr~ hehehe rubbies your bulgy wolgy you're so big :oooo rubbies more on your bulgy wolgy it doesn't stop growing ·///· kisses you and lickies your necky daddy likies (; nuzzles wuzzles I hope daddy really likes $: wiggles butt and squirms I want to see your big daddy meat~ wiggles butt I have a little itch o3o wags tail can you please get my itch~ puts paws on your chest nyea~ its a seven inch itch rubs your chest can you help me pwease squirms pwetty pwease sad face I need to be punished runs paws down your chest and bites lip like I need to be punished really good~ paws on your bulge as I lick my lips I'm getting thirsty. I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v licks shaft mmmm~ so musky drools all over your cock your daddy meat I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply oh god im so hard~ licks balls punish me daddy~ nyea~ squirms more and wiggles butt I love your musky goodness bites lip please punish me licks lips nyea~ suckles on your tip so good licks pre of your cock salty goodness~ eyes role back and goes balls deep mmmm~ moans and suckles");
+			} else if (regex_chase.test(message.content)) {
+				await message.channel.send(quote('chase_attractive')).catch(() => errorNotify("regex_chase", message.guild));
 			}
 
 			if (regex_kgb_space.test(message.content)) {
@@ -333,6 +336,20 @@ async function errorNotify(error, guild) {
 	} catch{
 		console.log('big problem with errorNotify');
 	}
+}
+
+function quote(choice) {
+	var output = "";
+	switch (choice) {
+		case 'chase_attractive':
+			output = "**From: God#5663**\n```be more attractive and i might be more susceptible to subjugation```";
+			break;
+		default:
+			output = "error with quote input";
+			console.log("Error with quote input");
+			break;
+	}
+	return output;
 }
 
 function createDebuff(debuffName) {
